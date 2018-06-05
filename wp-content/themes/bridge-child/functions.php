b6f0d0ca42ac05b14a856396d79eebf07e6ccf6c
NguyenKimDien <trietnk01@gmail.com>
2018-06-05 15:39:27 +0700

    cập nhật

--- wp-content/themes/bridge-child/functions.php
+++ wp-content/themes/bridge-child/functions.php
@@ -49,6 +49,16 @@ function do_output_buffer(){
 }
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
 add_action('init', 'do_output_buffer');
+/* begin menu */
+add_action('init', 'zendvn_theme_register_menus');
+function zendvn_theme_register_menus(){
+	register_nav_menus(
+		array(									
+			'category-product-menu' 			=> __('CategoryProductMenu'),			
+		)
+	);
+}
+/* end menu */
 /* begin ẩn menu */
 function vnkings_admin_menus() {   
    remove_menu_page( 'edit.php?post_type=portfolio_page' ); 
@@ -64,62 +74,6 @@ function vnkings_admin_menus() {
 }
 add_action( 'admin_menu', 'vnkings_admin_menus' );
 /* end ẩn menu */
-/* begin quick alo phone */
-add_action('wp_footer', 'footer_script_code');
-function footer_script_code(){
-	$script= '<div class="quick-alo-phone quick-alo-green quick-alo-show" id="quick-alo-phoneIcon" style="right: 10px; top: 20%;">
-  <a href="tel:097 340 80 07" title="Liên hệ nhanh">
-  <div class="quick-alo-ph-circle"></div>
-  <div class="quick-alo-ph-circle-fill"></div>
-  <div class="quick-alo-ph-img-circle"></div>
-  </a>
-</div>
-	';	
-	echo $script;
-}
-/* end quick alo phone */
-/* begin cong trinh tieu bieu */
-add_shortcode( 'cong_trinh_tieu_bieu', 'showFeaturedConstruction' );
-function showFeaturedConstruction(){
-	$args = array(		
-		'orderby' => 'date',
-		'order'   => 'DESC',
-		'posts_per_page'=>9     										
-	);  
-	$the_query = new WP_Query( $args );
-	if($the_query->have_posts()){
-		$k=1;
-		echo '<div>';
-		while ($the_query->have_posts()){
-			$the_query->the_post();     
-			$post_id=$the_query->post->ID;                          
-			$permalink=get_the_permalink($post_id);
-			$title=get_the_title($post_id);
-			$acreage=get_post_meta($post_id,"acreage",true);			
-			?>
-			<div class="vc_column_container vc_col-sm-4">
-				<div class="margin-right-15 margin-top-15 relative v2_bnc_pr_item">
-					<div><img src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>" /></div>
-					<div class="hfsaiiidsuh">
-						<a class="links_fixed" href="<?php echo $permalink; ?>"></a>
-						<div class="uweriuruiw">
-							<div class="lsouwuruwe"><?php echo $title; ?></div>
-							<div class="kjhgsruewi">Diện tích : căn hộ <?php echo $acreage; ?>m2</div>
-						</div>
-					</div>					
-				</div>				
-			</div>
-			<?php
-			if($k%3 == 0){
-				echo '<div class="clr"></div>';
-			}
-			$k++;
-		}
-		wp_reset_postdata();		
-		echo '</div>';
-	}
-}
-/* end cong trinh tieu bieu */
 /* begin fanpage */
 add_action('wp_footer', 'script_fanpage');
 function script_fanpage(){
@@ -134,230 +88,54 @@ function script_fanpage(){
 	echo $strScript;
 }
 /* end fanpage */
-/* begin lấy danh sách bài viết theo category */
-add_shortcode( 'list_article', 'loadListArticleByCategory' );
-function loadListArticleByCategory($atts){	
-	$att = shortcode_atts( array(
-		'taxonomy'=>'',
-		'post_type'=>'',
-		'term' => '',        
-	), $atts );
-	$taxonomy=trim($att['taxonomy']);	 
-	$post_type=trim($att['post_type']);	 
-	$term=trim($att['term']);
-	$data_term=explode( ',',$term);
-	global $zController,$zendvn_sp_settings;    
-	$vHtml=new HtmlControl();
-	$productModel=$zController->getModel("/frontend","ProductModel"); 
-	$args=array();
-	if(isset($_POST['q'])){
-		$q=$_POST['q'];
-		$args = array(
-		'post_type' => $post_type,  
-		'orderby' => 'date',
-		'order'   => 'DESC',  		 							
-		's' => $q
-	);
-	}else{
-		$args = array(
-		'post_type' => $post_type,  
-		'orderby' => 'date',
-		'order'   => 'DESC',  		 							
-		'tax_query' => array(
-			array(
-				'taxonomy' => $taxonomy,
-				'field'    => 'slug',
-				'terms'    => $data_term,									
-			),
-		),
-	);
-	}	  
-	$the_query = new WP_Query( $args );
-	$totalItemsPerPage=0;
-	$pageRange=10;
-	$currentPage=1; 
-	$totalItemsPerPage=get_option( 'posts_per_page' );    
-	if(!empty(@$_POST["filter_page"]))          {
-		$currentPage=@$_POST["filter_page"];  
-	}
-	$productModel->setWpQuery($the_query);   
-	$productModel->setPerpage($totalItemsPerPage);        
-	$productModel->prepare_items();               
-	$totalItems= $productModel->getTotalItems();               
-	$arrPagination=array(
-		"totalItems"=>$totalItems,
-		"totalItemsPerPage"=>$totalItemsPerPage,
-		"pageRange"=>$pageRange,
-		"currentPage"=>$currentPage   
-	);    
-	$pagination=$zController->getPagination("Pagination",$arrPagination); 
-	if($the_query->have_posts()){
-		$k=1;
-		echo '<form  method="post"  class="frm" name="frm">';
-		echo '<input type="hidden" name="filter_page" value="1" />';
-		while ($the_query->have_posts()){
-			$the_query->the_post();     
-			$post_id=$the_query->post->ID;                          
-			$permalink=get_the_permalink($post_id);
-			$title=get_the_title($post_id);
-			$acreage=get_post_meta($post_id,"acreage",true);			
-			?>
-			<div class="vc_column_container vc_col-sm-4">
-				<div class="margin-right-15 margin-top-15 relative v2_bnc_pr_item">
-					<div><img src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>" /></div>
-					<div class="hfsaiiidsuh">
-						<a class="links_fixed" href="<?php echo $permalink; ?>"></a>
-						<div class="uweriuruiw">
-							<div class="lsouwuruwe"><?php echo $title; ?></div>
-							<div class="kjhgsruewi">Diện tích : căn hộ <?php echo $acreage; ?>m2</div>
-						</div>
-					</div>					
-				</div>				
-			</div>
-			<?php
-			if($k%3 == 0){
-				echo '<div class="clr"></div>';
-			}
-			$k++;
-		}
-		wp_reset_postdata();
-		echo $pagination->showPagination();
-		echo '</form>';	
-		
-	}
-}
-/* end lấy danh sách bài viết theo category */
-/* begin social icon */
-add_shortcode('social_icon','showSocialIcon');
-function showSocialIcon(){
-	global $zendvn_sp_settings;	
-	$facebook_url=$zendvn_sp_settings['facebook_url'];
-	$twitter_url=$zendvn_sp_settings['twitter_url'];
-	$google_plus=$zendvn_sp_settings['google_plus'];
-	$youtube_url=$zendvn_sp_settings['youtube_url'];
-	?>
-	<div class="intubin">
-		<div class="relative">
-			<form name="frm-search" method="POST" action="/tim-kiem-du-an">
-				<input type="text" name="q" autocomplete="off" placeholder="Tìm kiếm dự án" value="">
-				<a href="javascript:void(0);" onclick="document.forms['frm-search'].submit();"><i class="fa fa-search" aria-hidden="true"></i></a>
-			</form>			
-		</div>
-		<div class="mioo">
-			<ul class="nicolas">
-				<li><a href="<?php echo $facebook_url; ?>" target="_blank"><i class="fab fa-facebook"></i></a></li>
-				<li><a href="<?php echo $twitter_url; ?>" target="_blank"><i class="fab fa-twitter-square"></i></a></li>
-				<li><a href="<?php echo $google_plus; ?>" target="_blank"><i class="fab fa-google-plus-square"></i></a></li>
-				<li><a href="<?php echo $youtube_url; ?>" target="_blank"><i class="fab fa-youtube-square"></i></a></li>
-			</ul>
-		</div>
-	</div>	
-	<?php
-}
-/* end social icon */
 /* begin search right */
 add_shortcode('search_right','showSearchRight');
 function showSearchRight(){
 	$terms = get_terms( array(
 		'taxonomy' => 'category_product',
-		'hide_empty' => false,  ) );			
+		'hide_empty' => false,  ) );	
+	$source_category=array();
+	$source_category[]=array('id'=>0,'name'=>'Tất cả danh mục');
+	foreach ($terms as $key => $value) {
+		$source_category[]=array('id'=>$value->term_id,'name'=>$value->name);
+	}		
 	?>	
-	<form name="frm-search" method="POST" class="ritae" action="/tim-kiem-du-an">
-		<div>
-			<select name="category_product" class="xima">
-				<?php 
-					foreach ($terms as $key => $value) {						
-						echo '<option value="'.$value->term_id.'">'.$value->name.'</option>';
-					}
-				?>
-			</select>
+	<div class="row">
+		<div class="col-lg-6">
+			<form name="frm-search" method="POST" class="ritae" action="/tim-kiem-du-an">
+				<div>
+					<select name="category_product" class="xima">
+						<?php 
+						foreach ($source_category as $key => $value) {						
+							echo '<option value="'.$value['id'].'">'.$value['name'].'</option>';
+						}
+						?>
+					</select>
+				</div>
+				<div>
+					<input type="text" name="q" class="lina" placeholder="Bạn cần gì hôm nay ?">
+				</div>
+				<div class="oppo">
+					<a href="javascript:void(0);" onclick="document.forms['frm-search'].submit();"><i class="fa fa-search" aria-hidden="true"></i></a>
+				</div>	
+			</form>
 		</div>
-		<div>
-			<input type="text" name="q" class="lina" placeholder="Bạn cần gì hôm nay ?">
+		<div class="col-lg-2">
+			<a class="kitae" href="javascript:void(0);">
+				<div class="icon-header"><i class="fas fa-cart-arrow-down"></i></div>
+				<div class="gian-ho"><font color="#ffffff">Giỏ hàng</font></div>
+			</a>
 		</div>
-		<div class="oppo">
-			<a href="javascript:void(0);" onclick="document.forms['frm-search'].submit();"><i class="fa fa-search" aria-hidden="true"></i></a>
-		</div>	
-		<a class="kitae" href="javascript:void(0);">
-			<div class="icon-header"><i class="fas fa-cart-arrow-down"></i></div>
-			<div class="gian-ho"><font color="#ffffff">Giỏ hàng</font></div>
-		</a>
-		<div class="lexa">
-			<div class="icon-header"><i class="fas fa-phone"></i></div>
-			<div>
-				<div><font color="#ffffff">1900.1267</font></div>
-				<div><font color="#ffffff">Hotline mua hàng</font></div>
-			</div>
-		</div>	
-	</form>
+		<div class="col-lg-4">
+			<div class="lexa">
+				<div class="icon-header"><i class="fas fa-phone"></i></div>
+				<div>
+					<div><font color="#ffffff">1900.1267</font></div>
+					<div><font color="#ffffff">Hotline mua hàng</font></div>
+				</div>
+			</div>	
+		</div>
+	</div>	
 	<?php
 }
 /* end search right */
-/* begin ảnh công trình */
-add_shortcode( 'show_album', 'showAlbum' );
-function showAlbum($atts){
-	$att = shortcode_atts( array(		
-		'post_type'=>'',		        
-	), $atts );	
-	$post_type=trim($att['post_type']);	
-	global $zController,$zendvn_sp_settings;    
-	$vHtml=new HtmlControl();
-	$productModel=$zController->getModel("/frontend","ProductModel"); 
-	$args = array(
-		'post_type' => $post_type,  
-		'orderby' => 'date',
-		'order'   => 'DESC',  		 								
-	);   
-	$the_query = new WP_Query( $args );
-	$totalItemsPerPage=0;
-	$pageRange=10;
-	$currentPage=1; 
-	$totalItemsPerPage=12;    
-	if(!empty(@$_POST["filter_page"]))          {
-		$currentPage=@$_POST["filter_page"];  
-	}
-	$productModel->setWpQuery($the_query);   
-	$productModel->setPerpage($totalItemsPerPage);        
-	$productModel->prepare_items();               
-	$totalItems= $productModel->getTotalItems();               
-	$arrPagination=array(
-		"totalItems"=>$totalItems,
-		"totalItemsPerPage"=>$totalItemsPerPage,
-		"pageRange"=>$pageRange,
-		"currentPage"=>$currentPage   
-	);    
-	$pagination=$zController->getPagination("Pagination",$arrPagination); 
-	if($the_query->have_posts()){
-		$k=1;
-		echo '<form  method="post"  class="frm" name="frm">';
-		echo '<input type="hidden" name="filter_page" value="1" />';
-		while ($the_query->have_posts()){
-			$the_query->the_post();     
-			$post_id=$the_query->post->ID;                          
-			$permalink=get_the_permalink($post_id);
-			$title=get_the_title($post_id);			
-			?>
-			<div class="vc_column_container vc_col-sm-3">
-				<div class="margin-right-15 margin-top-15 relative v2_bnc_pr_item">
-					<div><img src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>" /></div>
-					<div class="hfsaiiidsuh">
-						<a class="links_fixed" href="<?php echo $permalink; ?>"></a>
-						<div class="uweriuruiw">
-							<div class="lsouwuruwe"><?php echo $title; ?></div>							
-						</div>
-					</div>					
-				</div>				
-			</div>
-			<?php
-			if($k%4 == 0){
-				echo '<div class="clr"></div>';
-			}
-			$k++;
-		}
-		wp_reset_postdata();
-		echo $pagination->showPagination();
-		echo '</form>';	
-		
-	}
-}
-/* end ảnh công trình */
