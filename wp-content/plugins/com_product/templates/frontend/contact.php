350497776e6da0ee95a086f41e20c81f7a67f427
NguyenKimDien <trietnk01@gmail.com>
2018-06-14 17:20:41 +0700

    cap nhat

--- wp-content/plugins/com_product/templates/frontend/contact.php
+++
@@ -1,6 +0,0 @@
-<?php get_header();     ?>
-    <?php require_once PLUGIN_PATH . DS . "templates" . DS . "frontend". DS . "loop-contact.php"; ?>
-    <?php get_footer(); ?>
-    <?php wp_footer();?>
-</body>
-</html>
--- wp-content/themes/bridge-child/functions.php
+++ wp-content/themes/bridge-child/functions.php
@@ -446,10 +446,7 @@ function loadCategoryHome($attrs){
 												</div>
 												<div class="manufacturer-name margin-top-10"><a href="<?php echo $manufacturer_link; ?>"><?php echo $manufacturer_name; ?></a></div>
 												<div class="box-product-title margin-top-10"><a href="<?php echo $permalink; ?>" title="<?php echo $title; ?>" ><b><?php echo $title; ?></b></a></div>
-												<div class="margin-top-10"><?php echo $sku; ?></div>
-												<div class="box-product-intro margin-top-10">
-													<?php echo $intro; ?>
-												</div>
+												<div class="margin-top-10"><?php echo $sku; ?></div>												
 												<?php echo $html_price; ?>
 											</div>
 											<?php
