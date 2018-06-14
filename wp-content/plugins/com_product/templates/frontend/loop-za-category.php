98dce27c7473798d7f5977b39a68327a22dc5df8
NguyenKimDien <trietnk01@gmail.com>
2018-06-14 18:31:49 +0700

    cập nhật

--- wp-content/plugins/com_product/templates/frontend/loop-za-category.php
+++ wp-content/plugins/com_product/templates/frontend/loop-za-category.php
@@ -131,7 +131,7 @@
                             <?php echo $html_price; ?>
                             <div class="thia margin-top-10">
                                 <a href="javascript:void(0);" data-toggle="modal" data-target="#modal-alert-add-cart" onclick="javascript:addToCart(<?php echo $post_id; ?>,1);" >
-                                    <img width="120" src="<?php echo site_url('wp-content/uploads/mua-ngay.png'); ?>">                           
+                                    <img  src="<?php echo site_url('wp-content/uploads/mua-ngay.png'); ?>">                           
                                 </a>
                             </div>
                         </div>
