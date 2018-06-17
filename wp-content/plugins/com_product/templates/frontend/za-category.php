47410b41d5c2c2402bb0b59f2de8137893790afa
NguyenKimDien <trietnk01@gmail.com>
2018-06-18 01:48:20 +0700

    cập nhật

--- wp-content/plugins/com_product/templates/frontend/za-category.php
+++ wp-content/plugins/com_product/templates/frontend/za-category.php
@@ -19,19 +19,30 @@
 						</h1>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-lg-12">
-						<?php 						
-						$term_id = get_queried_object_id();
-						$source_termchildren = get_term_children( $term_id,'za_category');						
-						foreach ($source_termchildren as $key => $value) {
-							$term = get_term_by( 'id', $term_id, 'za_category');							
-							
-						}
-						?>	
+				<?php 						
+				$term_id = get_queried_object_id();
+				$source_termchildren = get_term_children( $term_id,'za_category');	
+				if(count($source_termchildren) > 0){
+					?>
+					<div class="row">
+						<div class="col-lg-12">			
+							<div class="lita">
+								<?php
+								foreach ($source_termchildren as $key => $value) {
+									$term = get_term_by( 'id', $value, 'za_category');												
+									$term_link= get_term_link($term,'za_category');
+									?>
+									<div><div class="niu"><a href="<?php echo $term_link; ?>"><?php echo $term->name; ?></a></div></div>
+									<?php 
+								}					
+								?>
+							</div>
+						</div>
 					</div>
-				</div>			
-				<?php require_once PLUGIN_PATH . DS . "templates" . DS . "frontend". DS . "loop-za-category.php"; ?>
+					<?php
+				}						
+				require_once PLUGIN_PATH . DS . "templates" . DS . "frontend". DS . "loop-za-category.php";
+				?>																					
 			</div>			
 		</div>
 	</div>
