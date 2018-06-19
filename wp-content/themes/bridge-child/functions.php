61c81439d67c511509fdb3b27d08591910d66e88
NguyenKimDien <trietnk01@gmail.com>
2018-06-19 09:51:03 +0700

    cập nhật

--- wp-content/themes/bridge-child/functions.php
+++ wp-content/themes/bridge-child/functions.php
@@ -101,18 +101,7 @@ function script_fanpage(){
     </div>
   </div>
 </div>  
-<div class="modal fade modal-invoice-report" id="modal-history-invoice" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>        
-      </div>
-      <div class="modal-body">
-        
-      </div>      
-    </div>
-  </div>
-</div>';
+';
 	echo $modal;
 	$tawk_to='<script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
